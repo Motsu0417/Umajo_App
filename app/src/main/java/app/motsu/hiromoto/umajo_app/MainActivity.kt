0147a388f017e2686897b51866e3153b27292d13
Motsu0417 <pippi9400@gmail.com>
2022-09-12 14:11:49 +0900

    Revert "first commit!"
    This reverts commit b892946d3e5fdf2521549656ab08ce26612f1c3b.

--- app/src/main/java/app/motsu/hiromoto/umajo_app/MainActivity.kt
+++ app/src/main/java/app/motsu/hiromoto/umajo_app/MainActivity.kt
@@ -2,14 +2,10 @@ package app.motsu.hiromoto.umajo_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import app.motsu.hiromoto.umajo_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var binding:ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
+        setContentView(R.layout.activity_main)
     }
 }
