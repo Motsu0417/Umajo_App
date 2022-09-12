b6b6f175e41a2c2ecb93371cd6bc995eaf72ecbc
Motsu0417 <pippi9400@gmail.com>
2022-09-12 14:12:19 +0900

    add:binding

--- app/src/main/java/app/motsu/hiromoto/umajo_app/MainActivity.kt
+++ app/src/main/java/app/motsu/hiromoto/umajo_app/MainActivity.kt
@@ -2,10 +2,14 @@ package app.motsu.hiromoto.umajo_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import app.motsu.hiromoto.umajo_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
     }
 }
