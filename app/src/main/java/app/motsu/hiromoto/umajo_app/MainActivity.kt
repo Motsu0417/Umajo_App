18043c8a241f4a3fdc02260e753167c2d34a1989
Motsu0417 <pippi9400@gmail.com>
2022-09-18 12:49:48 +0900

    add:Fragment

--- app/src/main/java/app/motsu/hiromoto/umajo_app/MainActivity.kt
+++ app/src/main/java/app/motsu/hiromoto/umajo_app/MainActivity.kt
@@ -2,6 +2,8 @@ package app.motsu.hiromoto.umajo_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
 import app.motsu.hiromoto.umajo_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +13,43 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
+        
+        binding.orderButton.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply{
+                replace(R.id.fragmentContainerView, OrderFragment(), "Order")
+            }.commit()
+            setButtonActive(false)
+        }
+
+        binding.timeButton.setOnClickListener{
+            supportFragmentManager.beginTransaction().apply{
+                replace(R.id.fragmentContainerView, TimeManageFragment(), "Time")
+            }.commit()
+            setButtonActive(false)
+        }
+
+        binding.workButton.setOnClickListener{
+            supportFragmentManager.beginTransaction().apply{
+                replace(R.id.fragmentContainerView, WorkManageFragment(), "Work")
+            }.commit()
+            setButtonActive(false)
+        }
+    }
+    
+    fun setButtonActive(boolean: Boolean){
+        binding.orderButton.isVisible = boolean
+        binding.timeButton.isVisible = boolean
+        binding.workButton.isVisible = boolean
+    }
+
+    override fun onBackPressed() {
+        supportFragmentManager.beginTransaction().apply{
+            for(fragment in supportFragmentManager.fragments)
+            remove(fragment)
+        }.commit()
+        setButtonActive(true)
+        if(supportFragmentManager.fragments.size == 0){
+            super.onBackPressed()
+        }
     }
 }
